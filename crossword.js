1a81ebc7dd5c7aa9bb929fc013cf3dad87581238
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-10-06 00:02:06 -0400

    changed to inputs regex not working

---
+++ crossword.js
@@ -0,0 +1,7 @@
+var cells = document.getElementsByClassName("cell");
+
+document.getElementsByClassName("cell").forEach((cell) => {
+  item.addEventListener("input", (event) => {
+    item.value = item.value.replace(/[^a-zA-Z]/);
+  });
+});
