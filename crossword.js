e02ea7ed920859c1d2950d2663134bad5e43b416
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-10-06 00:14:25 -0400

    fixed regex

--- crossword.js
+++
@@ -1,7 +0,0 @@
-var cells = document.getElementsByClassName("cell");
-
-document.getElementsByClassName("cell").forEach((cell) => {
-  item.addEventListener("input", (event) => {
-    item.value = item.value.replace(/[^a-zA-Z]/);
-  });
-});
