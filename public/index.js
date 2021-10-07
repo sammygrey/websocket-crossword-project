261316c9b4e1873db9100e3c70080dcc32056de9
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-10-06 23:46:34 -0400

    update format

---
+++ public/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const socket = io.connect();
+let currentUser;
+socket.emit("get online users");
+console.log(currentUser);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "sockets/index.js");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
