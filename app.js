0b0ba0efebbe65e99a020dab3216f0a6bd3bf2a5
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-10-07 02:41:45 -0400

    crossword.js to app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
 const express = require("express");
 const app = express();
+const path = require("path");
 const server = require("http").Server(app);
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname));
@@ -10,14 +11,37 @@ const io = require("socket.io")(server);
 let onlineUsers = {};
 let letters = {};
 
-io.on("connection", (socket) => {
-  require("./sockets/crossword.js")(io, socket, onlineUsers);
-});
+const correctValues = {
+  1: "a",
+  2: "a",
+  3: "a",
+  4: "a",
+  5: "a",
+  6: "a",
+  7: "a",
+  8: "a",
+  9: "a",
+  10: "a",
+  11: "a",
+  12: "a",
+  13: "a",
+  14: "a",
+  15: "a",
+  16: "a",
+};
 
 app.get("/", (req, res) => {
-  res.sendFile("public/crossword.html", { root: __dirname });
+  res.sendFile(path.join(__dirname + "/public/crossword.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("Socket.IO console connected!");
+  socket.on("cell change", (cell) => {
+    console.log(cell);
+    io.emit("cell change", cell);
+  });
 });
 
 server.listen(port, () => {
-  console.log(`Server listening on Port ${port}`);
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
