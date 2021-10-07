4b9f42508fd4dc08bae6ad8c421c17cd40cd4feb
guiisgooey <49354894+guiisgooey@users.noreply.github.com>
2021-10-07 01:58:10 -0400

    confirmation for if letter is correct

--- sockets/crossword.js
+++ sockets/crossword.js
@@ -1,6 +1,25 @@
+const correctValues = {
+  1: "a",
+  2: "a",
+  3: "a",
+  4: "a",
+  5: "a",
+  6: "a",
+  7: "a",
+  8: "a",
+  9: "a",
+  10: "a",
+  11: "a",
+  12: "a",
+  13: "a",
+  14: "a",
+  15: "a",
+  16: "a",
+};
+
 module.exports = (io, socket, onlineUsers) => {
   socket.on("cell", (data) => {
-    console.log(data);
+    console.log(Object.values(data)[0] == correctValues[Object.keys(data)[0]]);
   });
 
   socket.on("get online users", () => {
